5bbf893e243050deb398d732f0d0e5ee24149164
Okemwag <gabrielokemwa83@gmail.com>
2024-02-11 12:16:45 +0300

    Add autoprefixer, postcss, and tailwindcss dependencies*** ***Update accountsReducer to use Redux Toolkit*** ***Update index.css to use Tailwind CSS

--- client/src/redux/reducers/accountsReducer.ts
+++ client/src/redux/reducers/accountsReducer.ts
@@ -1,2 +1,36 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import accountsReducer from "./accountsReducer";
+import { Reducer } from 'redux';
+import { UserActionTypes, UserState, UserAction } from './types';
+
+const initialState: UserState = {
+    user: null,
+    loading: false,
+    error: null,
+};
+
+const userReducer: Reducer<UserState, UserAction> = (state = initialState, action) => {
+    switch (action.type) {
+        case UserActionTypes.FETCH_USER_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                error: null,
+            };
+        case UserActionTypes.FETCH_USER_SUCCESS:
+            return {
+                ...state,
+                user: action.payload,
+                loading: false,
+                error: null,
+            };
+        case UserActionTypes.FETCH_USER_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export default userReducer;
